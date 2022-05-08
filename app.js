8d1ea60e213118927760b7c06b27208b7d82175b
abhishek sahani <abhishek1sahani@gmail.com>
2022-05-08 21:04:53 +0530

    updating

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var app = express();
 
-app.listen(port,()=>{
-    console.log("Server is running on port 8080.")
-});
-
+// app.listen(port,()=>{
+//     console.log("Server is running on port 8080.")
+// });
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
+  
 const fs = require('fs');
 fs.readFile('./db.json','utf-8',(err,jsonString)=>{
     if (err){
